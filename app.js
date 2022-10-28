2d9f8e1fc0aac0c63e2380ce1eb463608afc390b
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-28 15:07:00 +0200

    Iteration1 complete

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 require('dotenv').config();
 
 const express = require('express');
-const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
 const app = express();
 
-app.set('view engine', 'hbs');
+const expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+// require spotify-web-api-node package here:
+const SpotifyWebApi = require("spotify-web-api-node");
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
 
 // Our routes go here:
 
